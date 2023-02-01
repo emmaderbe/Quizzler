736242c4e3abb35676b9622ed5e6cf5743f52d78
emmaderbe <emma-derbe@mail.ru>
2023-02-01 17:30:06 +0300

    first commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Дербе Эмма on 01.02.2023.
+//
+
+import Foundation
---
+++ Quizzler/QuizBrain.swift
@@ -0,0 +1,8 @@
+//
+//  QuizBrain.swift
+//  Quizzler
+//
+//  Created by Дербе Эмма on 01.02.2023.
+//
+
+import Foundation
